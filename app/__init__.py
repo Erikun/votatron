09993ac64b7d4f02a141b346a66e2dff5f14ff3a
Erik Melander <erik@helgo.net>
2019-01-26 15:59:39 +0100

    tabeller skapade, kanske till och med fungerar

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_pyfile('../config.py')
 db = SQLAlchemy(app)
-from .models import User, Vote
+from .models import User, Voting, Vote, Alternative
 db.create_all()
