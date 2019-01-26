b7dae6842dd710baa0311ed4330ddc7dc3508b38
Erik Melander <erik@helgo.net>
2019-01-26 16:12:50 +0100

    fixade saknad score i Alternative, rättat namn på kolumn i User och förlängt stränglängderna

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from . import db
 from datetime import datetime
 
 class User(db.Model):
-    username = db.Column(db.String(20), primary_key=True)
-    votes = db.relationship('Voting', backref='user', lazy=True)
+    username = db.Column(db.String(30), primary_key=True)
+    voting = db.relationship('Voting', backref='user', lazy=True)
     vote = db.relationship('Vote', backref='user', lazy=True)
 
     def __repr__(self):
@@ -13,7 +13,7 @@ class User(db.Model):
 class Voting(db.Model):
     id = db.Column(db.String, primary_key=True)
     creator = db.Column(db.String, db.ForeignKey('user.username'), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(300), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     nomination_end = db.Column(db.DateTime, default=None)
     voting_end = db.Column(db.DateTime)
@@ -33,5 +33,6 @@ class Vote(db.Model):
 class Alternative(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     voting_id = db.Column(db.String, db.ForeignKey('voting.id'), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
-    link = db.Column(db.String(100))
+    title = db.Column(db.String(300), nullable=False)
+    link = db.Column(db.String(300))
+    score = db.Column(db.Integer, default=0)
